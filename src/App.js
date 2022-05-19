e2c8c3f9f8257c9c687e4f7a6a5941777d6de488
christinraymond <christian1.raymond@gmail.com>
2022-05-19 23:30:59 +0200

    use react-router-dom v6 for this proj

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import Header from "./containers/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDeatils from "./containers/ProductDetails";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing/>} />
+          <Route path="/product/:productId" exact element={<ProductDeatils/>}/>
+          <Route>404 Not Found</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">FackShop</div>
+    </div>
+  );
+};
+
+export default Header;
