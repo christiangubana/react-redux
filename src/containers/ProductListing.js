c7648c8e7117b6c2a6caf8b2c680fa510b4a5750
christinraymond <christian1.raymond@gmail.com>
2022-05-20 10:44:28 +0200

    get product from the store using useSelector func

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux'
 
 const ProductListing = () => {
+  const products = useSelector((state) => state );
+  console.log(products)
   return (
-    <div>
+    <div className='ui grid container listing'>
       <h1>Product Listing</h1>
     </div>
   )
