372333320cf44924a9c8d4d0c0bba2d747b2357b
christinraymond <christian1.raymond@gmail.com>
2022-05-27 12:09:02 +0200

    can show single product dets on the UI

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -8,6 +8,7 @@ const ProductDetails = () => {
   const { productId } = useParams();
   const selectedProduct = useSelector((state) => state.product);
   const dispatch = useDispatch();
+  const { image, title, price, category, description } = selectedProduct;
 
   useEffect(() => {
     const fetchProductDetails = async () => {
@@ -22,8 +23,35 @@ const ProductDetails = () => {
   }, [dispatch, productId]);
   console.log(selectedProduct);
   return (
-    <div>
-      <h1>Product Details</h1>
+    <div className="ui grid container">
+      {Object.keys(selectedProduct).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            {/* <div className="ui vertical divider">AND</div> */}
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={image} alt={title}/>
+              </div>
+              <div className="column rp">
+                <h1>{title}</h1>
+                <h2>
+                  <a className="ui teal tag label" href={title}>${price}</a>
+                </h2>
+                <h3 className="ui brown block header">{category}</h3>
+                <p>{description}</p>
+                <div className="ui vertical animated button" tabIndex="0">
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
