a0d956f7c7774ac5c2184ce9553f66f4269c0a1c
christinraymond <christian1.raymond@gmail.com>
2022-05-27 11:48:40 +0200

    can dispatch single product to the store

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
@@ -7,18 +8,20 @@ const ProductComponent = () => {
     const { id, title, image, price, category } = product;
     return (
       <div className="four wide column" key={id}>
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image">
-              <img src={image} alt={title} />
-            </div>
-            <div className="content">
-              <div className="header">{title}</div>
-              <div className="meta price">$ {price}</div>
-              <div className="meta">{category}</div>
+        <Link to={`product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
   });
--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedProducts } from "../redux/actions/productActions";
 
 const ProductDetails = () => {
+  const { productId } = useParams();
+  const selectedProduct = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      const res = await axios
+        .get(`https://fakestoreapi.com/products/${productId}`)
+        .catch((err) => {
+          console.log("Error", err);
+        });
+      dispatch(selectedProducts(res.data));
+    };
+    if (productId && productId !== "") fetchProductDetails();
+  }, [dispatch, productId]);
+  console.log(selectedProduct);
   return (
     <div>
       <h1>Product Details</h1>
     </div>
-  )
-}
+  );
+};
 
 export default ProductDetails;
